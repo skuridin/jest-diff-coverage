07d5c166d9d3e04d9ad5ab589ea17340ecf3977e
Evgeniy Skuridin <redfield.ru@gmail.com>
2020-03-26 11:47:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require("@actions/code");
+const child = require("child_process");
+
+function main() {
+  const testCommand = core.getInput("command");
+
+  child.execSync(testCommand);
+  const coveragePercentage = child
+    .execSync("npx coverage-percentage ./coverage/lcov.info --lcov")
+    .toString();
+
+  if (parseFloat(coveragePercentage) < 80) {
+    core.setFailed("Not enough coverage");
+  }
+}
+
+try {
+  main();
+} catch (error) {
+  core.setFailed(error.message);
+}
